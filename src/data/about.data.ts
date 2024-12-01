ab059cd4e53d81e815acb1e80a488a4a93b1612d
thisIsBun <bun.coding@gmail.com>
2024-12-01 12:51:36 +0800

    update: skills and resume

--- src/data/about.data.ts
+++ src/data/about.data.ts
@@ -2,14 +2,16 @@ type ToolNames = string[];
 
 const aboutData: { tools: ToolNames } = {
   tools: [
-    "JavaScript (ES6+)",
-    "HTML & CSS",
-    "React",
-    "Styled-Components",
-    "TypeScript",
-    "Storybook",
-    "Vite",
-    "i18next",
+    'JavaScript(ES6+)',
+    'HTML & CSS',
+    'React.js',
+    'Styled-Components',
+    'TypeScript',
+    'Storybook',
+    'Vite',
+    'i18next',
+    'Git',
+    'GitHub Actions',
   ],
 };
 
