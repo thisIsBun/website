1eb88ad0a4b59a17559ec98e9eb6467569eb30ac
thisIsBun <bun.coding@gmail.com>
2024-12-15 16:57:43 +0800

    update: experiences wording and resume file

--- src/data/experiences.data.tsx
+++ src/data/experiences.data.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Strong from '../components/fonts/Strong.style';
-import Anchor from '../components/others/Anchor.style';
 
 type experienceDataType = {
   company: string;
@@ -16,25 +14,11 @@ const experienceData: experienceDataType[] = [
     title: 'Engineer',
     period: 'May 2023 — Present',
     description: [
-      <>
-        <Strong>Collaborate</Strong> with PMs, backend engineers, and QA teams
-        to deliver functional, user-friendly digital interfaces within a
-        one-month agile cycle.
-      </>,
-      <>
-        <Strong>Engage</Strong> in requirement discussions, analyze user
-        workflows, and proactively address potential challenges to boost
-        development efficiency and product quality.
-      </>,
-      <>
-        <Strong>Document</Strong> system designs with detailed requirement
-        backgrounds and change scopes, ensuring clarity and providing reliable
-        references for future development.
-      </>,
-      <>
-        <Strong>Refactor</Strong> and optimize key areas of the codebase to
-        enhance product quality, maintainability, and stability.
-      </>,
+      'Developed React.js platforms for B2B clients, and built reusable UI components using Styled Components and Storybook.',
+      'Migrated the build tool from Webpack to Vite by referencing official documentation, reducing server startup time and improving development efficiency.',
+      'Studied the details of WCAG 2.1 guidelines and analyzed the project codebase to evaluate the adjustments needed for compliance with accessibility standards.',
+      'Documented development process with detailed requirements and change scopes to ensure alignment with project needs, enhancing development quality.',
+      'Proactively discussed requirements with teams, ensured alignment with customer needs, and resolved issues early to streamline development.',
     ],
     tools: ['JavaScript', 'React', 'Storybook', 'Vite', 'Docker'],
   },
@@ -43,40 +27,28 @@ const experienceData: experienceDataType[] = [
     title: 'Software Product Manager',
     period: 'Jun 2021 — May 2023',
     description: [
-      'Responsible for QVR-Human(crowd flow analysis), and QVR-Face(face recognition) software development.',
-      'Collaborated with UI/UX designers to evaluate feasibility with frontend and backend engineers, assisting QA in defining tests.',
-      'Defined MVP, wrote PRD, and provided marketing materials.',
+      'Led development of crowd flow analysis and facial recognition software.',
+      'Partnered with UI/UX designers and engineers to evaluate feasibility and optimize user experience.',
+      'Authored detailed MVPs, PRDs, and marketing content to support software launches',
     ],
     tools: ['Product Design', 'Cross-functional Collaborations'],
   },
   {
-    company: 'Asus',
+    company: 'ASUS',
     title: 'Software Project Manager',
     period: 'Apr 2020 — Jun 2021',
     description: [
-      'Lead the task force team to improve software stability.',
-      'Gathered user issues from various countries, supported QA in reproducing issues.',
+      'Led a task force to enhance software stability by collaborating with global key users to clarify and reproduce issues based on their usage scenarios.',
+      'Presented at the annual global sales meeting, earning recognition from the sales team for significant improvements in software stability.',
     ],
     tools: ['Software Troubleshooting', 'Cross-functional Collaborations'],
   },
   {
-    company: 'Asus',
+    company: 'ASUS',
     title: 'Hardware Product Manager',
     period: 'May 2015 — Apr 2020',
     description: [
-      'Responsible for the ROG gaming keyboard and mouse development.',
-      <>
-        Won the German{' '}
-        <Anchor
-          to='https://ifdesign.com/en/winner-ranking/project/cerberus-fortus/234226'
-          target
-          underline
-          ariaLabel='if Design award website'
-        >
-          iF Design Award
-        </Anchor>{' '}
-        in 2018.
-      </>,
+      'Led development of ROG gaming keyboards and mice, earning the 2018 German iF Design Award.',
     ],
     tools: ['Product Development', 'Cross-functional Collaborations'],
   },
